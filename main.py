9498d13fefc9193f489e26f68066b6db5c90ab8c
zaeyonz <wodyd723@gmail.com>
2026-02-17 23:19:13 +0900

    PD, LGD, EAD, EL 계산

--- main.py
+++ main.py
@@ -18,14 +18,17 @@ print("data:", df.shape)
 y = df["SeriousDlqin2yrs"]
 X = df.drop(columns=["SeriousDlqin2yrs"])
 
+# fill na to median 
 X = X.fillna(X.median())
 
+# train, test split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
+# train model 
 model = LogisticRegression(max_iter=2000)
 model.fit(X_train, y_train)
 
-
+# extract 
 y_prob = model.predict_proba(X_test)[:,1]
 
 auc = roc_auc_score(y_test, y_prob)
